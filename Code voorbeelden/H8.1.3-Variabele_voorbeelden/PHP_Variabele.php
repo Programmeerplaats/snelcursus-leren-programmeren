b0fc9ede65d3d2703c9067ef84a517145200c4dd
Bas Dingemans <BasD92@gmail.com>
2019-11-04 11:10:25 +0100

    Opening en closing tag toegevoegd aan php code

--- Code voorbeelden/H8.1.3-Variabele_voorbeelden/PHP_Variabele.php
+++ Code voorbeelden/H8.1.3-Variabele_voorbeelden/PHP_Variabele.php
@@ -1,6 +1,10 @@
-$nummer1 = 27;    
-$nummer2 = 91;  
+<?php
+
+    $nummer1 = 27;    
+    $nummer2 = 91;  
 	
-$resultaat = $nummer1 + $nummer2;  
+    $resultaat = $nummer1 + $nummer2;  
 	
-echo "Resultaat van optelling: ". $resultaat;
+    echo "Resultaat van optelling: ". $resultaat;
+    
+?>
