fdea5e0604876de16ed9ebc24109b94b43d44f22
Bas Dingemans <BasD92@gmail.com>
2019-11-04 15:15:47 +0100

    Spatie fixes

--- Code voorbeelden/H8.2.4-Array_loop_voorbeelden/Java_Array-while-loop.java
+++ Code voorbeelden/H8.2.4-Array_loop_voorbeelden/Java_Array-while-loop.java
@@ -1,8 +1,8 @@
 public class MyClass {    
 	public static void main(String args[]) {    
 			
-		int cijfers[]={4,6,2,5};
-		int index=0;
+		int cijfers[] = {4,6,2,5};
+		int index = 0;
 			
 		while(index<4){    
 			System.out.println(cijfers[index]);   
