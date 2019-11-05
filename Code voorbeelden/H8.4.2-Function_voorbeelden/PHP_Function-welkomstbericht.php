f5623ad08234beed91eac3fc5967991e93b5a1ea
Bas Dingemans <BasD92@gmail.com>
2019-11-05 16:33:15 +0100

    Opening en closing tags toegevoegd aan PHP code

--- Code voorbeelden/H8.4.2-Function_voorbeelden/PHP_Function-welkomstbericht.php
+++ Code voorbeelden/H8.4.2-Function_voorbeelden/PHP_Function-welkomstbericht.php
@@ -1,7 +1,11 @@
-$voornaam = "Henk";  
-  
-function welkomstbericht($voornaamGebruiker) {  
-	echo "Welkom " . $voornaamGebruiker . ", leuk dat je er bent!";   
-}  
-  
-welkomstbericht($voornaam);
+<?php
+
+	$voornaam = "Henk";  
+
+	function welkomstbericht($voornaamGebruiker) {  
+		echo "Welkom " . $voornaamGebruiker . ", leuk dat je er bent!";   
+	}  
+
+	welkomstbericht($voornaam);
+
+?>
--- Code voorbeelden/H8.5.2-Commentaar_voorbeelden/PHP_Commentaar.php
+++ Code voorbeelden/H8.5.2-Commentaar_voorbeelden/PHP_Commentaar.php
@@ -1,19 +1,23 @@
-/* 
-Welkomstbericht aan gebruiker 
-door middel van een function 
-*/  
-  
-// Variabele initialiseren  
-$voornaam = "Henk";    
-  
-/* 
-Function waarin bezoeker welkom 
-wordt geheten. $voornaamGebruiker 
-is de parameter. 
-*/  
-function welkomstbericht($voornaamGebruiker) {    
-  echo "Welkom " . $voornaamGebruiker . ", leuk dat je er bent!";     
-}    
-  
-# Function aanroepen met $voornaam als argument  
-welkomstbericht($voornaam);
+<?php
+
+  /* 
+  Welkomstbericht aan gebruiker 
+  door middel van een function 
+  */  
+    
+  // Variabele initialiseren  
+  $voornaam = "Henk";    
+    
+  /* 
+  Function waarin bezoeker welkom 
+  wordt geheten. $voornaamGebruiker 
+  is de parameter. 
+  */  
+  function welkomstbericht($voornaamGebruiker) {    
+    echo "Welkom " . $voornaamGebruiker . ", leuk dat je er bent!";     
+  }    
+    
+  # Function aanroepen met $voornaam als argument  
+  welkomstbericht($voornaam);
+
+?>
