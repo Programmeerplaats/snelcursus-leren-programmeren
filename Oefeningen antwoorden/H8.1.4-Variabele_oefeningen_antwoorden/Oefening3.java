e7be3f4b1250de850f35f4800bebdd8ad65dcf18
Bas Dingemans <BasD92@gmail.com>
2021-01-15 15:11:23 +0100

    Leeftijd aangepast

--- Oefeningen antwoorden/H8.1.4-Variabele_oefeningen_antwoorden/Oefening3.java
+++ Oefeningen antwoorden/H8.1.4-Variabele_oefeningen_antwoorden/Oefening3.java
@@ -2,7 +2,7 @@ public class MyClass {
 	public static void main(String args[]) {    
   
 		String naam = "Bas";    
-		int leeftijd = 25;    
+		int leeftijd = 28;    
   
 		String beschrijving = "Ik ben " + naam + " en ik ben " + leeftijd + " jaar oud. Mijn hobby's zijn: Programmeren en voetbal.";    
   
